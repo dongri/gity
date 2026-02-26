ee314793935bda4e2a323ecae55377ffe958f1cf
Sergey Chelak <sergey.chelak@gmail.com>
2026-02-26 23:53:23 +0200

    persist clear recent list

--- Sources/AppState.swift
+++ Sources/AppState.swift
@@ -20,26 +20,32 @@ class AppState: ObservableObject {
     func setup() {
         loadRecentRepositories()
     }
-    
-    private func loadRecentRepositories() {
-        if let data = UserDefaults.standard.data(forKey: "recentRepositories"),
-           let urls = try? JSONDecoder().decode([URL].self, from: data) {
-            recentRepositories = urls
-        }
-    }
-    
+        
     func addRecentRepository(_ url: URL) {
         recentRepositories.removeAll { $0 == url }
         recentRepositories.insert(url, at: 0)
         if recentRepositories.count > 10 {
             recentRepositories = Array(recentRepositories.prefix(10))
         }
-        if let data = try? JSONEncoder().encode(recentRepositories) {
-            UserDefaults.standard.set(data, forKey: "recentRepositories")
-        }
+        saveRecentRepositoriesList()
     }
     
     func clearRecentRepositories() {
         recentRepositories.removeAll()
+        saveRecentRepositoriesList()
+    }
+    
+    // MARK: utils for load/store data
+    private func loadRecentRepositories() {
+        if let data = UserDefaults.standard.data(forKey: "recentRepositories"),
+           let urls = try? JSONDecoder().decode([URL].self, from: data) {
+            recentRepositories = urls
+        }
+    }
+
+    private func saveRecentRepositoriesList() {
+        if let data = try? JSONEncoder().encode(recentRepositories) {
+            UserDefaults.standard.set(data, forKey: "recentRepositories")
+        }
     }
 }
